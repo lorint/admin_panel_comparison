8908cd2824343cc87042a3ca849f029441599da0
Lorin Thwaits <lorint@gmail.com>
2023-03-07 04:52:13 +0000

    Have Brick create an inline <img> element for Category

--- app/avo/resources/category_resource.rb
+++ app/avo/resources/category_resource.rb
@@ -2,15 +2,16 @@ class CategoryResource < Avo::BaseResource
   self.title = :category_name
   self.includes = [:products]
   self.record_selector = false
-  self.description = "Category picture is a random image from https://picsum.photos/ just for the exmaple."
+  self.description = "Category picture is rendered from binary data stored in the database."
 
   field :category_name, as: :text
   field :description, as: :trix, always_show: true
   field :description_excerpt, as: :text, only_on: :index do |category|
     ActionView::Base.full_sanitizer.sanitize(category.description).truncate 120
   end
-  field :picture, as: :external_image do |category|
-    "https://picsum.photos/400"
+  field :picture, as: :text do |category|
+    # Have Brick create an <img> element using inline Base64 when there is a picture available
+    ::Brick::Rails.display_binary(category.picture).html_safe
   rescue
     nil
   end
